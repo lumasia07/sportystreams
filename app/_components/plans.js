c2a15cb10d96877ec6eff73fc0275e5c425644ca
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-07-30 19:59:39 +0300

    plans section

---
+++ app/_components/plans.js
@@ -0,0 +1,15 @@
+import { Kodchasan } from "next/font/google";
+
+const kodchasan = Kodchasan({ weight: ["400"], subsets: ["latin"] });
+
+export default function Plans() {
+    return (
+        <div className={`${kodchasan.className} bg-[#061231] flex items-center`}>
+            <div className="border-t-4 rounded-lg w-2/5 ml-4 md:ml-24"></div>
+            <div className="mx-3 flex-grow text-center text-2xl font-bold md:text-4xl mb-3 bg-gradient-to-r from-red-500 to-pink-500 bg-clip-text text-transparent">
+                plans
+            </div>
+            <div className="border-t-4 rounded-lg w-2/5 mr-4 md:mr-24"></div>
+        </div>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Navbar from "./_components/nav";
 import Header from "./_components/header";
+import Plans from "./_components/plans";
 
 export default function Homepage() {
     return (
         <>
           <Navbar />
           <Header />
+          <Plans />
         </>
     );
 }
