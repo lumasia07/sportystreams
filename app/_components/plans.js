e83f0f1fd2c90c70cb08afc7652435be0c65f860
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-07-30 20:16:44 +0300

    added a plans div with a gradient

--- app/_components/plans.js
+++ app/_components/plans.js
@@ -4,12 +4,17 @@ const kodchasan = Kodchasan({ weight: ["400"], subsets: ["latin"] });
 
 export default function Plans() {
     return (
-        <div className={`${kodchasan.className} bg-[#061231] pt-5 flex items-center`}>
-            <div className="border-t-4 rounded-lg w-2/5 ml-4 md:ml-24"></div>
-            <div className="mx-3 flex-grow text-center text-2xl font-bold md:text-4xl mb-3 bg-gradient-to-r from-red-500 to-pink-500 bg-clip-text text-transparent">
-                plans
+        <div className={`${kodchasan.className} bg-[#061231] pt-5 flex flex-col items-center`}>
+            <div className="flex items-center w-full">
+                <div className="border-t-4 rounded-lg w-2/5 ml-4 md:ml-24"></div>
+                <div className="mx-3 flex-grow text-center text-2xl font-bold md:text-4xl mb-3 bg-gradient-to-r from-red-500 to-pink-500 bg-clip-text text-transparent">
+                    plans
+                </div>
+                <div className="border-t-4 rounded-lg w-2/5 mr-4 md:mr-24"></div>
             </div>
-            <div className="border-t-4 rounded-lg w-2/5 mr-4 md:mr-24"></div>
+            <div className="border m-12 p-4 rounded-lg bg-gradient-to-r from-red-500 to-pink-500">
+                Here are the plans 
+            </div>
         </div>
     );
 }
