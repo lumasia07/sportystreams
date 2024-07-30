ce588e2f81b0aca84a13e1a623da6006005e999d
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-07-30 20:02:19 +0300

    increased padding top

--- app/_components/plans.js
+++ app/_components/plans.js
@@ -4,7 +4,7 @@ const kodchasan = Kodchasan({ weight: ["400"], subsets: ["latin"] });
 
 export default function Plans() {
     return (
-        <div className={`${kodchasan.className} bg-[#061231] flex items-center`}>
+        <div className={`${kodchasan.className} bg-[#061231] pt-5 flex items-center`}>
             <div className="border-t-4 rounded-lg w-2/5 ml-4 md:ml-24"></div>
             <div className="mx-3 flex-grow text-center text-2xl font-bold md:text-4xl mb-3 bg-gradient-to-r from-red-500 to-pink-500 bg-clip-text text-transparent">
                 plans
