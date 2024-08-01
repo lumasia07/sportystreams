dd0f255ec816e2820f268dedf0c467d6aeb030ce
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-08-01 03:42:02 +0300

    nav bar fix

--- app/_components/nav.js
+++ app/_components/nav.js
@@ -10,12 +10,12 @@ export default function Navbar() {
 
   return (
     <div className="p-4 flex items-center justify-between bg-[#061231] text-white">
-      <div className={`${radio.className} pl-2 md:pl-20`}>
+      <div className={`${radio.className} pl-2 md:pl-16`}>
         <h1 className="text-2xl md:text-3xl font-bold">SportyStreams</h1>
       </div>
       <div className="flex-grow hidden md:flex justify-center">
-        <div className="border border-[#061231] rounded-md py-4 px-10 bg-[#071842] shadow-2xl">
-          <ul className="flex space-x-12 list-none">
+        <div className="border border-[#061231] rounded-md py-6 px-8 bg-[#071842] shadow-2xl">
+          <ul className="flex space-x-8 list-none text-sm md:text-base">
             {menu_options.map((option, index) => (
               <li key={index} className="hover:text-[#FF2828] cursor-pointer transition-colors duration-200">
                 {option}
@@ -24,7 +24,7 @@ export default function Navbar() {
           </ul>
         </div>
       </div>
-      <div className="hidden md:flex space-x-6 mr-16">
+      <div className="hidden md:flex space-x-6 mr-12">
         <button className="border-2 text-white px-6 py-2 rounded-full hover:bg-[#FF2828] transition-all duration-200">
           Account
         </button>
